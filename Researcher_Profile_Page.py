56532962b7c1aa01efdf3f0473525df5fa6a0982
Tsepo206 <97953641+Tsepo206@users.noreply.github.com>
2025-01-29 14:44:24 +0200

    Update Researcher_Profile_Page.py

--- Researcher_Profile_Page.py
+++ Researcher_Profile_Page.py
@@ -8,7 +8,7 @@ st.title("Tsepo Sekhoasha Profile Page")
 # Collect basic information
 name = "Tsepo Sekhaosha"
 field = "Astrophysics"
-institution = "University of KwaZulu-Natal (UKZ)"
+institution = "University of KwaZulu-Natal (UKZN)"
 
 # Display basic profile information
 st.header("Researcher Overview")
